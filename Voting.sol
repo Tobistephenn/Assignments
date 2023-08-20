a693e9751121ef49298a982483bd68083006df69
Tobi Stephen <77818984+Tobistephenn@users.noreply.github.com>
2023-08-20 01:42:12 +0100

    Create Voting.sol

---
+++ Voting.sol
@@ -0,0 +1,23 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
+
+contract votingsystem {
+  address public owner;
+  mapping(string => uint256) public votes
+	
+	constructo
+	r(){
+	owner = msg.sender;
+ 	}
+	modifier onlyOwner(){
+ 	require(msg.sender == owner, "only the owner can perform this action");
+	_;
+ 	}
+	function voteForCandidate(string memory candidate) external {
+ 	require(bytes(candidate).length > 0, "candidate name must not be empty)";
+	votes[candidate]++;
+	}
+ function getVotesForCandidate(string memory candidate) external view returns (uint256){
+ return votes[candidate];
+ }
+}
