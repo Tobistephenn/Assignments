5a3b12ef755ff2c0cc15266506a9b992bcb42694
Tobi Stephen <77818984+Tobistephenn@users.noreply.github.com>
2023-08-20 02:45:03 +0100

    Update Voting.sol

--- Voting.sol
+++ Voting.sol
@@ -1,3 +1,4 @@
+// Voting System
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.3;
 
@@ -19,4 +20,32 @@ contract votingsystem {
  	function getVotesForCandidate(string memory candidate) external view returns (uint256){
  	return votes[candidate];
  	}
+}
+
+
+// Bank Mechanism
+contract bankmechanism {
+	address public owner;
+	mapping(string => uint256) public balances;
+
+	constructor() {
+	owner = msg.sender;
+	}
+	modifier onlyOwner() {
+	require(msg.sender == owner, "Only the owner can perform this action");
+	_;
+	}
+	function deposit() external payable {
+	balances[msg.sender] += msg.value;
+	}
+	function withdraw(uint256 amount) external {
+	require(amount > 0, "Amount must be greater than 0);
+	require(balances[msg.sender] >= amount);
+
+	balances[msg.sender] -= amount;
+	payable(msg.sender).transfer(amount);
+	}
+	function getBalance() external view returns (uint256) {
+	return balances[msg.sender];
+	}
 }
